828a8837ef76c3a520742e1529c57e3ee45a37ae
fannyhou2024 <fannyhou00@yahoo.com.hk>
2025-12-18 11:01:09 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,12 +1,25 @@
-import streamlit as st
+# import streamlit as st
+# import plotly.express as px
+# import plotly.io as pio
+# # pio.renderers.default = "browser"
+# df = px.data.iris()
+# print(df.head())
+# fig = px.scatter(df, x='sepal_width',
+# y='sepal_length',
+# color='species',
+# size='petal_length',
+# hover_data=['petal_width'])
+# fig.show()
+
 import plotly.express as px
-import plotly.io as pio
-pio.renderers.default = "browser"
-df = px.data.iris()
-print(df.head())
-fig = px.scatter(df, x='sepal_width',
-y='sepal_length',
-color='species',
-size='petal_length',
-hover_data=['petal_width'])
-fig.show()
+import streamlit as st
+import pandas as pd
+
+# Load data
+df = px.data.gapminder()
+
+# Create figure
+fig = px.scatter(df, x='year', y='lifeExp', color='continent')
+
+# Use Streamlit's native command to show the plot
+st.plotly_chart(fig)
